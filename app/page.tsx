3e0602d052740bfb54428e778bc60e89b152b59b
Nikola Pavlovic <nikola.pavlovic@uzh.ch>
2025-03-20 12:07:19 +0100

    Vercel test

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 import "@ant-design/v5-patch-for-react-19";
-import { useRouter } from "next/navigation";
-import Image from "next/image";
-import { Button } from "antd";
-import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
-import styles from "@/styles/page.module.css";
+//import { useRouter } from "next/navigation";
+//import Image from "next/image";
+//import { Button } from "antd";
+//import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
+//import styles from "@/styles/page.module.css";
 
-export default function Home() {
+/*export default function Home() {
   const router = useRouter();
   return (
     <div className={styles.page}>
@@ -114,4 +114,4 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+}*/
